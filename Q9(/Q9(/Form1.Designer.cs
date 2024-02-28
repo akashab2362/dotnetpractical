4abfc76e57ca72389ad8a6aa506e1d177cbd9f15
abhishek <abhishek123@gmail.com>
2024-02-28 07:41:44 +0530

    Q9-a completed

---
+++ Q9(/Q9(/Form1.Designer.cs
@@ -0,0 +1,547 @@
+﻿namespace Q9_
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            panel1 = new Panel();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            label4 = new Label();
+            textBox2 = new TextBox();
+            label5 = new Label();
+            label6 = new Label();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            label7 = new Label();
+            label8 = new Label();
+            textBox5 = new TextBox();
+            label9 = new Label();
+            label10 = new Label();
+            dateTimePicker2 = new DateTimePicker();
+            dateTimePicker3 = new DateTimePicker();
+            label11 = new Label();
+            label12 = new Label();
+            label13 = new Label();
+            label14 = new Label();
+            label15 = new Label();
+            textBox6 = new TextBox();
+            label16 = new Label();
+            label17 = new Label();
+            label18 = new Label();
+            label19 = new Label();
+            textBox7 = new TextBox();
+            textBox8 = new TextBox();
+            textBox9 = new TextBox();
+            textBox10 = new TextBox();
+            label20 = new Label();
+            textBox11 = new TextBox();
+            textBox12 = new TextBox();
+            textBox13 = new TextBox();
+            label21 = new Label();
+            textBox14 = new TextBox();
+            textBox15 = new TextBox();
+            textBox16 = new TextBox();
+            label22 = new Label();
+            textBox17 = new TextBox();
+            label23 = new Label();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackgroundImage = (Image)resources.GetObject("panel1.BackgroundImage");
+            panel1.Controls.Add(label1);
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1227, 150);
+            panel1.TabIndex = 0;
+            panel1.Paint += panel1_Paint;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = SystemColors.ActiveCaptionText;
+            label1.Font = new Font("Showcard Gothic", 28F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = SystemColors.ControlLightLight;
+            label1.Location = new Point(301, 82);
+            label1.Name = "label1";
+            label1.Size = new Size(614, 68);
+            label1.TabIndex = 0;
+            label1.Text = "Rental Application";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 167);
+            label2.Name = "label2";
+            label2.Size = new Size(178, 25);
+            label2.TabIndex = 1;
+            label2.Text = "Appartment Address";
+            label2.Click += label2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.ForeColor = Color.IndianRed;
+            label3.Location = new Point(185, 167);
+            label3.Name = "label3";
+            label3.Size = new Size(20, 25);
+            label3.TabIndex = 2;
+            label3.Text = "*";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(12, 195);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(947, 31);
+            textBox1.TabIndex = 3;
+            textBox1.TextChanged += this.textBox1_TextChanged;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(12, 244);
+            label4.Name = "label4";
+            label4.Size = new Size(128, 25);
+            label4.TabIndex = 4;
+            label4.Text = "Address Line 2";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(12, 272);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(947, 31);
+            textBox2.TabIndex = 5;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(12, 322);
+            label5.Name = "label5";
+            label5.Size = new Size(42, 25);
+            label5.TabIndex = 6;
+            label5.Text = "City";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.ForeColor = Color.IndianRed;
+            label6.Location = new Point(47, 322);
+            label6.Name = "label6";
+            label6.Size = new Size(20, 25);
+            label6.TabIndex = 7;
+            label6.Text = "*";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(12, 350);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(279, 31);
+            textBox3.TabIndex = 8;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(301, 350);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(279, 31);
+            textBox4.TabIndex = 11;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.ForeColor = Color.IndianRed;
+            label7.Location = new Point(344, 322);
+            label7.Name = "label7";
+            label7.Size = new Size(20, 25);
+            label7.TabIndex = 10;
+            label7.Text = "*";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(301, 322);
+            label8.Name = "label8";
+            label8.Size = new Size(51, 25);
+            label8.TabIndex = 9;
+            label8.Text = "State";
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(586, 350);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(279, 31);
+            textBox5.TabIndex = 14;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.ForeColor = Color.IndianRed;
+            label9.Location = new Point(663, 322);
+            label9.Name = "label9";
+            label9.Size = new Size(20, 25);
+            label9.TabIndex = 13;
+            label9.Text = "*";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(586, 322);
+            label10.Name = "label10";
+            label10.Size = new Size(84, 25);
+            label10.TabIndex = 12;
+            label10.Text = "Zip Code";
+            // 
+            // dateTimePicker2
+            // 
+            dateTimePicker2.Location = new Point(344, 428);
+            dateTimePicker2.Name = "dateTimePicker2";
+            dateTimePicker2.Size = new Size(300, 31);
+            dateTimePicker2.TabIndex = 16;
+            // 
+            // dateTimePicker3
+            // 
+            dateTimePicker3.Enabled = false;
+            dateTimePicker3.Location = new Point(12, 428);
+            dateTimePicker3.Name = "dateTimePicker3";
+            dateTimePicker3.Size = new Size(300, 31);
+            dateTimePicker3.TabIndex = 19;
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.ForeColor = Color.IndianRed;
+            label11.Location = new Point(457, 400);
+            label11.Name = "label11";
+            label11.Size = new Size(20, 25);
+            label11.TabIndex = 18;
+            label11.Text = "*";
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Location = new Point(344, 400);
+            label12.Name = "label12";
+            label12.Size = new Size(120, 25);
+            label12.TabIndex = 17;
+            label12.Text = "Move-in Date";
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.ForeColor = Color.IndianRed;
+            label13.Location = new Point(126, 400);
+            label13.Name = "label13";
+            label13.Size = new Size(20, 25);
+            label13.TabIndex = 21;
+            label13.Text = "*";
+            // 
+            // label14
+            // 
+            label14.AutoSize = true;
+            label14.Location = new Point(12, 400);
+            label14.Name = "label14";
+            label14.Size = new Size(113, 25);
+            label14.TabIndex = 20;
+            label14.Text = "Today's Date";
+            // 
+            // label15
+            // 
+            label15.AutoSize = true;
+            label15.Location = new Point(12, 475);
+            label15.Name = "label15";
+            label15.Size = new Size(96, 25);
+            label15.TabIndex = 22;
+            label15.Text = "Occupants";
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(68, 538);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(150, 31);
+            textBox6.TabIndex = 23;
+            // 
+            // label16
+            // 
+            label16.AutoSize = true;
+            label16.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label16.Location = new Point(68, 500);
+            label16.Name = "label16";
+            label16.Size = new Size(62, 25);
+            label16.TabIndex = 24;
+            label16.Text = "Name";
+            // 
+            // label17
+            // 
+            label17.AutoSize = true;
+            label17.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label17.Location = new Point(250, 500);
+            label17.Name = "label17";
+            label17.Size = new Size(66, 25);
+            label17.TabIndex = 25;
+            label17.Text = "Phone";
+            // 
+            // label18
+            // 
+            label18.AutoSize = true;
+            label18.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label18.Location = new Point(423, 500);
+            label18.Name = "label18";
+            label18.Size = new Size(58, 25);
+            label18.TabIndex = 26;
+            label18.Text = "Email";
+            // 
+            // label19
+            // 
+            label19.AutoSize = true;
+            label19.Location = new Point(12, 538);
+            label19.Name = "label19";
+            label19.Size = new Size(22, 25);
+            label19.TabIndex = 27;
+            label19.Text = "1";
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(250, 538);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(150, 31);
+            textBox7.TabIndex = 28;
+            // 
+            // textBox8
+            // 
+            textBox8.Location = new Point(423, 538);
+            textBox8.Name = "textBox8";
+            textBox8.Size = new Size(150, 31);
+            textBox8.TabIndex = 29;
+            // 
+            // textBox9
+            // 
+            textBox9.Location = new Point(423, 575);
+            textBox9.Name = "textBox9";
+            textBox9.Size = new Size(150, 31);
+            textBox9.TabIndex = 33;
+            // 
+            // textBox10
+            // 
+            textBox10.Location = new Point(250, 575);
+            textBox10.Name = "textBox10";
+            textBox10.Size = new Size(150, 31);
+            textBox10.TabIndex = 32;
+            // 
+            // label20
+            // 
+            label20.AutoSize = true;
+            label20.Location = new Point(12, 575);
+            label20.Name = "label20";
+            label20.Size = new Size(22, 25);
+            label20.TabIndex = 31;
+            label20.Text = "2";
+            // 
+            // textBox11
+            // 
+            textBox11.Location = new Point(68, 575);
+            textBox11.Name = "textBox11";
+            textBox11.Size = new Size(150, 31);
+            textBox11.TabIndex = 30;
+            // 
+            // textBox12
+            // 
+            textBox12.Location = new Point(423, 612);
+            textBox12.Name = "textBox12";
+            textBox12.Size = new Size(150, 31);
+            textBox12.TabIndex = 37;
+            // 
+            // textBox13
+            // 
+            textBox13.Location = new Point(250, 612);
+            textBox13.Name = "textBox13";
+            textBox13.Size = new Size(150, 31);
+            textBox13.TabIndex = 36;
+            // 
+            // label21
+            // 
+            label21.AutoSize = true;
+            label21.Location = new Point(12, 612);
+            label21.Name = "label21";
+            label21.Size = new Size(22, 25);
+            label21.TabIndex = 35;
+            label21.Text = "3";
+            // 
+            // textBox14
+            // 
+            textBox14.Location = new Point(68, 612);
+            textBox14.Name = "textBox14";
+            textBox14.Size = new Size(150, 31);
+            textBox14.TabIndex = 34;
+            // 
+            // textBox15
+            // 
+            textBox15.Location = new Point(423, 649);
+            textBox15.Name = "textBox15";
+            textBox15.Size = new Size(150, 31);
+            textBox15.TabIndex = 41;
+            // 
+            // textBox16
+            // 
+            textBox16.Location = new Point(250, 649);
+            textBox16.Name = "textBox16";
+            textBox16.Size = new Size(150, 31);
+            textBox16.TabIndex = 40;
+            // 
+            // label22
+            // 
+            label22.AutoSize = true;
+            label22.Location = new Point(12, 649);
+            label22.Name = "label22";
+            label22.Size = new Size(22, 25);
+            label22.TabIndex = 39;
+            label22.Text = "4";
+            // 
+            // textBox17
+            // 
+            textBox17.Location = new Point(68, 649);
+            textBox17.Name = "textBox17";
+            textBox17.Size = new Size(150, 31);
+            textBox17.TabIndex = 38;
+            // 
+            // label23
+            // 
+            label23.AutoSize = true;
+            label23.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label23.Location = new Point(12, 707);
+            label23.Name = "label23";
+            label23.Size = new Size(469, 25);
+            label23.TabIndex = 42;
+            label23.Text = "Have any occupants ever been evicted  from a rental?";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1227, 758);
+            Controls.Add(label23);
+            Controls.Add(textBox15);
+            Controls.Add(textBox16);
+            Controls.Add(label22);
+            Controls.Add(textBox17);
+            Controls.Add(textBox12);
+            Controls.Add(textBox13);
+            Controls.Add(label21);
+            Controls.Add(textBox14);
+            Controls.Add(textBox9);
+            Controls.Add(textBox10);
+            Controls.Add(label20);
+            Controls.Add(textBox11);
+            Controls.Add(textBox8);
+            Controls.Add(textBox7);
+            Controls.Add(label19);
+            Controls.Add(label18);
+            Controls.Add(label17);
+            Controls.Add(label16);
+            Controls.Add(textBox6);
+            Controls.Add(label15);
+            Controls.Add(label13);
+            Controls.Add(label14);
+            Controls.Add(dateTimePicker3);
+            Controls.Add(label11);
+            Controls.Add(label12);
+            Controls.Add(dateTimePicker2);
+            Controls.Add(textBox5);
+            Controls.Add(label9);
+            Controls.Add(label10);
+            Controls.Add(textBox4);
+            Controls.Add(label7);
+            Controls.Add(label8);
+            Controls.Add(textBox3);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(textBox2);
+            Controls.Add(label4);
+            Controls.Add(textBox1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(panel1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += this.Form1_Load;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox1;
+        private Label label4;
+        private TextBox textBox2;
+        private Label label5;
+        private Label label6;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private Label label7;
+        private Label label8;
+        private TextBox textBox5;
+        private Label label9;
+        private Label label10;
+        private DateTimePicker dateTimePicker2;
+        private DateTimePicker dateTimePicker3;
+        private Label label11;
+        private Label label12;
+        private Label label13;
+        private Label label14;
+        private Label label15;
+        private TextBox textBox6;
+        private Label label16;
+        private Label label17;
+        private Label label18;
+        private Label label19;
+        private TextBox textBox7;
+        private TextBox textBox8;
+        private TextBox textBox9;
+        private TextBox textBox10;
+        private Label label20;
+        private TextBox textBox11;
+        private TextBox textBox12;
+        private TextBox textBox13;
+        private Label label21;
+        private TextBox textBox14;
+        private TextBox textBox15;
+        private TextBox textBox16;
+        private Label label22;
+        private TextBox textBox17;
+        private Label label23;
+    }
+}
---
+++ Q9(/Q9(/Form1.cs
@@ -0,0 +1,20 @@
+namespace Q9_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
